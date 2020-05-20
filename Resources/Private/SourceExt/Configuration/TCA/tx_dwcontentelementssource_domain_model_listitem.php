19e8754d1954a5a7bf88b955a2725813ce4c7139
Sascha Zander <sascha.zander@denkwerk.com>
2020-05-20 19:50:37 +0200

    Update source extension example TCA config for database strict mode setups and set version to "1.8.1"

--- Resources/Private/SourceExt/Configuration/TCA/tx_dwcontentelementssource_domain_model_listitem.php
+++ Resources/Private/SourceExt/Configuration/TCA/tx_dwcontentelementssource_domain_model_listitem.php
@@ -75,16 +75,19 @@ return [
         'sys_language_uid' => array(
             'exclude' => 1,
             'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
-            'config' => array(
+            'config' => [
                 'type' => 'select',
                 'renderType' => 'selectSingle',
-                'foreign_table' => 'sys_language',
-                'foreign_table_where' => 'ORDER BY sys_language.title',
-                'items' => array(
-                    array('LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages', -1),
-                    array('LLL:EXT:lang/locallang_general.xlf:LGL.default_value', 0)
-                )
-            )
+                'special' => 'languages',
+                'items' => [
+                    [
+                        'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.allLanguages',
+                        -1,
+                        'flags-multiple'
+                    ],
+                ],
+                'default' => 0,
+            ]
         ),
         'l10n_parent' => array(
             'displayCond' => 'FIELD:sys_language_uid:>:0',
@@ -98,6 +101,7 @@ return [
                 'foreign_table' => 'tx_dwcontentelementssource_domain_model_listitem',
                 'foreign_table_where' => 'AND tx_dwcontentelementssource_domain_model_listitem.pid=###CURRENT_PID### 
                     AND tx_dwcontentelementssource_domain_model_listitem.sys_language_uid IN (-1,0)',
+                'default' => 0,
             ),
         ),
         'l10n_diffsource' => array(

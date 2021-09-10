a9dca0be41e5df2d803c1ce51586b650d5ce97a0
Sascha Zander <sascha.zander@denkwerk.com>
2021-09-10 16:54:53 +0200

    Raise requirement condition for TYPO3 version 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF['dw_content_elements'] = array(
     'priority' => 'top',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.4.1-10.4.99',
+            'typo3' => '11.4.1-11.4.99',
             'extbase' => '',
         ),
         'conflicts' => array(),
